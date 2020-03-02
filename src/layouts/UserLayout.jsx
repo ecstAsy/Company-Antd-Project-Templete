d6a78642798bb916afef7bb556ad47ec91171fe2
hexiaoqiang <hexiaoqiang@cketrip.com>
2020-03-02 15:03:16 +0800

    antd template

---
+++ src/layouts/UserLayout.jsx
@@ -0,0 +1,63 @@
+import { DefaultFooter, getMenuData, getPageTitle } from '@ant-design/pro-layout';
+import { Helmet } from 'react-helmet';
+import { Link } from 'umi';
+import React from 'react';
+import { formatMessage } from 'umi-plugin-react/locale';
+import { connect } from 'dva';
+import logo from '../assets/logo.svg';
+import styles from './UserLayout.less';
+
+const UserLayout = props => {
+  const {
+    route = {
+      routes: [],
+    },
+  } = props;
+  const { routes = [] } = route;
+  const {
+    children,
+    location = {
+      pathname: '',
+    },
+  } = props;
+  const { breadcrumb } = getMenuData(routes);
+  const title = getPageTitle({
+    pathname: location.pathname,
+    formatMessage,
+    breadcrumb,
+    ...props,
+  });
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={title} />
+      </Helmet>
+
+      <div className={styles.container}>
+        <div className={styles.lang} />
+        <div className={styles.content}>
+          <div className={styles.top}>
+            <div className={styles.header}>
+              <Link to="/">
+                <img alt="logo" className={styles.logo} src={logo} />
+                <span className={styles.title}>Ant Design</span>
+              </Link>
+            </div>
+          </div>
+          {children}
+        </div>
+        <DefaultFooter
+          links={false}
+          copyright={
+            <>
+              2020 中德联信
+            </>
+          }
+        />
+      </div>
+    </>
+  );
+};
+
+export default connect(({ settings }) => ({ ...settings }))(UserLayout);
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { Card } from 'antd';
+import styles from './Welcome.less';
+
+export default () => (
+  <PageHeaderWrapper>
+    <Card>
+      ddd
+    </Card>
+  </PageHeaderWrapper>
+);
